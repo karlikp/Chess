a5bdec549f62f34d0a17f442bb56620cd3b7474c
karlikp <pitera.karol@gmail.com>
2023-11-13 22:27:47 +0100

    Adding new function - read_start_position() with validation user-input

--- Chess/Functions.cpp
+++ Chess/Functions.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <string>
 #include <fstream>
+#include <utility>
+#include <cctype>
 
 #include "functions.h"
 
@@ -39,13 +41,70 @@ void single_move(Player name)
 	}
 }
 
+pair<char, int> read_start_position()
+{
+	bool correct;
+	char upper_letter{};
+	int number;
+	do
+	{
+		char letter;
+		string coordinates;
+		correct = true;
+
+		cout << "\nType in coordinate of your piece, which you're going to do a move"
+			<< " (for example: D2) ";
+
+
+		getline(cin, coordinates);
+		letter = coordinates[0];
+		number = int(coordinates[1] - '0');
+
+		coordinates.clear();
+
+		upper_letter = toupper(letter);
+		if (not (upper_letter <= int('H') and upper_letter >= int('A')))
+		{
+			cout << "\nYour first sign is wrong";
+			correct = false;
+		}
+		if (not (number <= 8 and number >= 1))
+		{
+			cout << "\nYour second sign is wrong";
+			correct = false;
+		}
+		if (not correct)
+			cout << "\nCorrect your answer, please.";
+	} while (not correct);
+
+	pair<char, int> checked_coordinates(upper_letter, number);
+	return checked_coordinates;
+
+}
+
+//pair<char, int> piece_destination()
+//{
+//
+//}
+
 pair<string, string> move_decision()
 {
 	string start, destination;
-	cout << "\nCheck your piece, which you' re going to do move (e.q. C3): ";
+
+	
 	getline(cin, start);
-	cout << "\nCheck your piece, which you' re going to do move (e.q. C3): ";
+	//function check correctness
+	cout << "\nType in the coordinate where you're going to shift the chosen piece (e.q. D4): ";
+	getline(cin, destination);
+	//function check correctness
+	pair <string, string> move;
+	move.first = start;
+	move.second = destination;
+
+	return move;
 }
+
+
 
 
 
--- Chess/Functions.h
+++ Chess/Functions.h
@@ -16,7 +16,7 @@ using namespace std;
 vector<string>read_string_from_file(string file_name); 
 
 void single_move(Player);
-
 pair<string,string> move_decision();
+pair<char, int> read_start_position();
 
 
